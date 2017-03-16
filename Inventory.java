03008919bca9d43b2b614d5bd63accb6cfe89512
Rithdoc <Rithdoc@users.noreply.github.com>
2017-03-15 20:41:10 -0600

    Tutuorial step 6, Closing the program correctly, step 1 complete!

--- Inventory.java
+++ Inventory.java
@@ -15,7 +15,6 @@ public class Inventory extends Application{
 
     Stage window;
     Button button;
-    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -26,41 +25,25 @@ public class Inventory extends Application{
         //Our Main JavaFX code will go here! :)
 
         window = primaryStage;
-        window.setTitle("This is Rithdoc's execution of boston's tutorial!");
+        window.setTitle("Rithdoc's JavaFX newboston tutorial code");
 
-        //Label label1 = new Label("Welcome to the first scene!");
+        window.setOnCloseRequest(e -> closeProgram());
 
-        button = new Button("Click me like Alice did!");
-        button.setOnAction(e -> {
-            boolean result = Part.display("Windows Title", "Are you sure you want to do that, Dave?");
-            System.out.println(result);
-        });
+        button = new Button("Close Program");
+        button.setOnAction(e -> closeProgram());
 
         StackPane layout = new StackPane();
         layout.getChildren().add(button);
         Scene scene = new Scene(layout, 300, 250);
 
-//        //Button 1
-//        Button button1 = new Button("Go to scene 2");
-//        button1.setOnAction(e -> window.setScene(scene2));
-//
-//        //Layout 1 - children laid out in vertical column
-//        VBox layout1 = new VBox(20);
-//        layout1.getChildren().addAll(label1, button1);
-//        scene1 = new Scene(layout1, 200, 200);
-//
-//        //Button 2
-//        Button button2 = new Button("This scene sucks, go back to scene 1");
-//        button2.setOnAction(e -> window.setScene(scene1));
-//
-//        //Layout 2
-//        StackPane layout2 = new StackPane();
-//        layout2.getChildren().add(button2);
-//        scene2 = new Scene(layout2, 600, 300);
-
         window.setScene(scene);
         window.show();
 
+    }
+
+    private void closeProgram(){
+        System.out.println("This code ran.");
+        window.close();
     }
 
 
