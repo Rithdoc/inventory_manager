76fb9457660e894985e13561b4962a2b98384a04
Rithdoc <Rithdoc@users.noreply.github.com>
2017-03-15 19:29:07 -0600

    Step 5 of tutorial, making an alertbox.  Not followed strictly, as I'm converting this project slowly for another purpose

--- Inventory.java
+++ Inventory.java
@@ -14,6 +14,7 @@ import java.awt.*;
 public class Inventory extends Application{
 
     Stage window;
+    Button button;
     Scene scene1, scene2;
 
     public static void main(String[] args) {
@@ -25,28 +26,36 @@ public class Inventory extends Application{
         //Our Main JavaFX code will go here! :)
 
         window = primaryStage;
-        Label label1 = new Label("Welcome to the first scene!");
+        window.setTitle("This is Rithdoc's execution of boston's tutorial!");
 
-        //Button 1
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(e -> window.setScene(scene2));
+        //Label label1 = new Label("Welcome to the first scene!");
 
-        //Layout 1 - children laid out in vertical column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-        scene1 = new Scene(layout1, 200, 200);
+        button = new Button("Click me like Alice did!");
+        button.setOnAction(e -> Product.display("My Alertbox in my Product class", "Wow, this alertbox is awesome!"));
 
-        //Button 2
-        Button button2 = new Button("This scene sucks, go back to scene 1");
-        button2.setOnAction(e -> window.setScene(scene1));
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        
+//        //Button 1
+//        Button button1 = new Button("Go to scene 2");
+//        button1.setOnAction(e -> window.setScene(scene2));
+//
+//        //Layout 1 - children laid out in vertical column
+//        VBox layout1 = new VBox(20);
+//        layout1.getChildren().addAll(label1, button1);
+//        scene1 = new Scene(layout1, 200, 200);
+//
+//        //Button 2
+//        Button button2 = new Button("This scene sucks, go back to scene 1");
+//        button2.setOnAction(e -> window.setScene(scene1));
+//
+//        //Layout 2
+//        StackPane layout2 = new StackPane();
+//        layout2.getChildren().add(button2);
+//        scene2 = new Scene(layout2, 600, 300);
 
-        //Layout 2
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 600, 300);
-
-        window.setScene(scene1);
-        window.setTitle("This is the Title of the Stage!");
+        window.setScene(scene);
         window.show();
 
     }
