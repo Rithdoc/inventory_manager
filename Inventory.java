2fb707e75410cf88edc4b33c2ff7773fe90aa241
Rithdoc <Rithdoc@users.noreply.github.com>
2017-03-15 18:51:43 -0600

    Finished tutorial for Scene Switching in this commit

--- Inventory.java
+++ Inventory.java
@@ -1,39 +1,62 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 /**
  * Created by Shaun on 3/14/2017.
  */
 public class Inventory extends Application{
 
-    Button button;
+    //Button button;
 
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World");
+    Stage window;
+    Scene scene1, scene2;
+
+    public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         //Our Main JavaFX code will go here! :)
 
-        primaryStage.setTitle("Title of the Window");
+        window = primaryStage;
+        Label label1 = new Label("Welcome to the first scene!");
 
-        button = new Button("First Text on the Button");
-        button.setText("Click Me, like Alice did!");
-        button.setOnAction(e -> System.out.println("That's me, the lambda expression version!"));
+        //Button 1
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+//        primaryStage.setTitle("Title of the Window");
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+//        button = new Button("First Text on the Button");
+//        button.setText("Click Me, like Alice did!");
+//        button.setOnAction(e -> System.out.println("That's me, the lambda expression version!"));
+
+        //Layout 1 - children laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+        //Button 2
+        Button button2 = new Button("This scene sucks, go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        //Layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        //Scene scene = new Scene(layout, 300, 250);
+        window.setScene(scene1);
+        window.setTitle("This is the Title of the Stage!");
+        window.show();
 
     }
 
