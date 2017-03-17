9cac2a19efeabecb70dba0d65761715b02ce3885
Rithdoc <Rithdoc@users.noreply.github.com>
2017-03-17 00:37:56 -0600

    Tutorial 11, Checkboxes, Subway sandwiches!

--- Inventory.java
+++ Inventory.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.geometry.*;
 import sun.rmi.runtime.Log;
+import javafx.scene.control.CheckBox;
 
 import java.awt.*;
 
@@ -30,26 +31,61 @@ public class Inventory extends Application{
         //Our Main JavaFX code will go here! :)
 
         window = primaryStage;
-        window.setTitle("Rithdoc's JavaFX Messabout");
-
+        window.setTitle("Checkbox Example");
 
         //Form
         TextField nameInput = new TextField("Rithdoc");
-        button = new Button("Click me");
-        button.setOnAction(e -> isInt(nameInput, nameInput.getText()));
 
-        GridPane.setConstraints(nameInput, 1, 0);
+        //CheckBoxes
+        CheckBox box1 = new CheckBox("Bacon");
+        CheckBox box2 = new CheckBox("Tuna");
+        CheckBox box3 = new CheckBox("Spinach");
+        CheckBox box4 = new CheckBox("Chicken");
+        CheckBox box5 = new CheckBox("Cheese");
+        box5.setSelected(true);
+
+        //Button
+        button = new Button("Order Now!");
+        button.setOnAction(e -> handleOptions(box1, box2, box3, box4, box5));
 
 
         //Layout
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(nameInput, button);
+        layout.getChildren().addAll(box1, box2, box3, box4, box5, button);
 
         scene = new Scene(layout, 300, 200);
         window.setScene(scene);
         window.show();
 
+    }
+
+    //Handle Checkbox Options
+    private void handleOptions(CheckBox box1, CheckBox box2, CheckBox box3, CheckBox box4, CheckBox box5){
+        String message = "User's order:\n";
+
+        if(box1.isSelected())
+        {
+            message += "Bacon\n";
+        }
+        if(box2.isSelected())
+        {
+            message += "Tuna\n";
+        }
+        if(box3.isSelected())
+        {
+            message += "Spinach\n";
+        }
+        if(box4.isSelected())
+        {
+            message += "Chicken\n";
+        }
+        if(box5.isSelected())
+        {
+            message += "Cheese\n";
+        }
+
+        System.out.println(message);
     }
 
     private boolean isInt(TextField input, String message)
